d65da71becd6b6e30c6840c03c206524997c8bc8
Ronak Luhar <ronakmethan@gmail.com>
2020-05-20 08:47:57 +0530

    test: home

--- cypress/integration/e2e/home.js
+++ cypress/integration/e2e/home.js
@@ -4,6 +4,20 @@ describe('Home', () => {
   })
 
   it('Should have banner', () => {
-    cy.get()
+    cy.get('h1').should('have.text', 'Cheese n Corn')
+  })
+
+  it('Should have products', () => {
+    cy.get('div').should('have.class', 'pizza-list')
+    cy.get('div').should('have.class', 'pizza-card')
+  })
+
+  it('Should have add to cart button', () => {
+    cy.get('button').should('have.class', 'add-to-cart')
+  })
+
+  it('Should add to cart', () => {
+    cy.get('button.add-to-cart').first().click()
+    cy.get('button.add-to-cart').first().should('have.class', 'disabled')
   })
 })
